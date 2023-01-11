0554c7156b2a95ec100f45dc07fabdc1430cf5ff
ilia <ilia.volkov2016@mail.ru>
2023-01-11 19:45:18 +0300

    Урок 3. Более простые методы

--- app/src/main/java/com/iliavolkov/popularlibraries/RxJava.kt
+++ app/src/main/java/com/iliavolkov/popularlibraries/RxJava.kt
@@ -1,39 +1,34 @@
 package com.iliavolkov.popularlibraries
 
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.disposables.Disposable
 
 
 private val  data = listOf(1,2,3,4,5,5,5,3,67)
 
 fun main(){
-    val observer = object : Observer<Int>{
-        override fun onSubscribe(d: Disposable) = Unit
-
-        override fun onNext(t: Int) {
-            println("Okey: $t")
-        }
-
-        override fun onError(e: Throwable) {
-            println("Error")
-        }
-
-        override fun onComplete() {
-            println("Finish")
-        }
-    }
-
-    val observable = Observable.create(){ emitter ->
-        try {
-            data.forEach { item ->
-                emitter.onNext(item)
+    Observable.fromIterable(data)
+        .subscribe(
+            {element->
+                println(element)
+            },
+            { error ->
+                println(error)
+            },
+            {
+                println("sd")
             }
-        } catch (e:Throwable){
-            emitter.onError(e)
-        }
-        emitter.onComplete()
-    }
 
-    observable.subscribe(observer)
+        )
+
+    Observable.fromCallable{
+        val result = old()
+        return@fromCallable result
+    }.subscribe{
+        println(it)
+    }
+}
+
+fun old(): List<Int> {
+    Thread.sleep(3000)
+    return listOf(4,6,8)
 }
