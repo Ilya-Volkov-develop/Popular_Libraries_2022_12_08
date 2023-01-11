a68e64cdbd4afbf66a7a048193e63ed717beb1f9
ilia <ilia.volkov2016@mail.ru>
2023-01-11 20:05:45 +0300

    Урок 3. Операторы

--- app/src/main/java/com/iliavolkov/popularlibraries/RxJava.kt
+++ app/src/main/java/com/iliavolkov/popularlibraries/RxJava.kt
@@ -3,10 +3,23 @@ package com.iliavolkov.popularlibraries
 import io.reactivex.rxjava3.core.Observable
 
 
-private val  data = listOf(1,2,3,4,5,5,5,3,67)
+private val  data = listOf(1,2,3,4,5,5,5,3,67,5)
 
 fun main(){
     Observable.fromIterable(data)
+        .filter{ it <= 5} //обыкновенный фильтр
+        .skip(3) //пропускает первые 3
+        .take(6) //первые 3
+        .map{
+            it + 2 //изменения со всеми элементами
+        }
+        .map(::mapItemToList)
+        .map {
+            it.add(8)
+            return@map it
+        }
+        .distinct() //удаляет повторяющиеся элементы
+        .distinctUntilChanged() //удаляет повторяющиеся элементы если они стоят рядом
         .subscribe(
             {element->
                 println(element)
@@ -17,18 +30,6 @@ fun main(){
             {
                 println("sd")
             }
-
         )
-
-    Observable.fromCallable{
-        val result = old()
-        return@fromCallable result
-    }.subscribe{
-        println(it)
-    }
 }
-
-fun old(): List<Int> {
-    Thread.sleep(3000)
-    return listOf(4,6,8)
-}
+private fun mapItemToList(value:Int) = mutableListOf(value, 9)
