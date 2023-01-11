0b1126d249a4eff986c4b2bbcc287197d0016953
ilia <ilia.volkov2016@mail.ru>
2023-01-11 20:38:58 +0300

    Урок 3. Операторы

--- app/src/main/java/com/iliavolkov/popularlibraries/RxJava.kt
+++ app/src/main/java/com/iliavolkov/popularlibraries/RxJava.kt
@@ -1,35 +1,31 @@
 package com.iliavolkov.popularlibraries
 
 import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
+import kotlin.random.Random
 
 
-private val  data = listOf(1,2,3,4,5,5,5,3,67,5)
+private val data = listOf(1, 2, 3, 4, 5, 5, 5, 3, 67, 5)
 
-fun main(){
-    Observable.fromIterable(data)
-        .filter{ it <= 5} //обыкновенный фильтр
-        .skip(3) //пропускает первые 3
-        .take(6) //первые 3
-        .map{
-            it + 2 //изменения со всеми элементами
+fun main() {
+
+    val observableName = Observable.just("Jonny", "Ilia", "Vika")
+    val observableMail = Observable.just("Jonny@mail.ru", "Ilia@mail.com",)
+
+    Observable.zip(observableName,observableMail){ name, mail ->
+        return@zip "$name : $mail"
+    }.subscribe{
+        println(it)
+    }
+
+    observableName
+        .flatMap { element ->
+            val delay = Random.nextInt(1000)
+            return@flatMap Observable.just(element)
+                .delay (delay.toLong(),TimeUnit.MILLISECONDS)
         }
-        .map(::mapItemToList)
-        .map {
-            it.add(8)
-            return@map it
+//        .observeOn(mainThread) //Переключение на главный поток в android
+        .subscribe {
+            println(it)
         }
-        .distinct() //удаляет повторяющиеся элементы
-        .distinctUntilChanged() //удаляет повторяющиеся элементы если они стоят рядом
-        .subscribe(
-            {element->
-                println(element)
-            },
-            { error ->
-                println(error)
-            },
-            {
-                println("sd")
-            }
-        )
 }
-private fun mapItemToList(value:Int) = mutableListOf(value, 9)
